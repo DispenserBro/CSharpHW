a1f8b58c871d5ac278051356c3bf45ffe02bb805
Danil Iarosh <maze3290@gmail.com>
2022-11-07 22:23:51 +0300

    readded some elements of homework

--- HW_4/Program.cs
+++ HW_4/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Task 1 / Задача 25
 // Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+// That's what all will do:
 /*
 int PowerNumber(int number, int power)
 {
@@ -15,6 +16,17 @@ Console.Write("Enter a number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter a natural power: ");
 int power = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{num}^{power} equals {PowerNumber(num, power)}");
+*/
+
+// That's what we need to do:
+
+// Same method as above
+/*
+Console.Write("Enter a number and power with comma and space (like \"a, b\"): ");
+string[] input = Console.ReadLine().Split(", ");
+int num = Convert.ToInt32(input[0]);
+int pow = Convert.ToInt32(input[1]);
 Console.WriteLine($"{num}^{power} equals {PowerNumber(num, power)}");
 */
 
@@ -42,6 +54,7 @@ Console.WriteLine($"Sum of digits in {mySuperCoolNumber} equals {SumDigits(mySup
 
 // Task 3 / Задача 29
 // Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+// That's what all will do (any of below methods):
 
 // Fixed elements count
 /*
@@ -72,6 +85,7 @@ ShowArray(mySuperDuperArray);
 */
 
 // Custom elements count
+
 int[] ReturnEnteredArray(int size)
 {
     int[] array = new int[size];
@@ -104,3 +118,30 @@ int arraySize = Convert.ToInt32(Console.ReadLine());
 
 int[] mySuperDuperArray = ReturnEnteredArray(arraySize);
 ShowArray(mySuperDuperArray);
+
+
+// That's what we need to do:
+/*
+int[] ConvertStrArrToIntArr(string[] array)
+{
+    int[] newArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+        newArray[i] = Convert.ToInt32(array[i]);
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    foreach (int el in array)
+        Console.Write($"{el} ");
+
+    Console.WriteLine("\nThat's all folks!");
+}
+
+Console.Write("Enter elements of array with comma and space (like \"a, b\"): ");
+string[] input = Console.ReadLine().Split(", ");
+
+int[] mySuperDuperArray = ConvertStrArrToIntArr(input);
+
+ShowArray(mySuperDuperArray);
+*/
